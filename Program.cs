879ebf8d6999ac3f914a54f985c73920e52846e0
wasif789 <wsffarid1@gmail.com>
2021-11-29 16:21:10 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO_PayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO payroll service");
+        }
+    }
+}
